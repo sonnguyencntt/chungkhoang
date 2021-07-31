86aeb53feb98a00a19bdb446337e02112f657972
= <=>
2021-07-31 10:29:31 +0700

    CONTROLLER V01

---
+++ app/Repositories/Blog/BlogRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories\Blog;
+
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class BlogRepository.
+ */
+class BlogRepository extends BaseRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/','HomeController@index')->name('home.index');
+Route::get('/gioi-thieu','IntroduceController@index')->name('introduce.index');
+Route::get('/lien-he','ContactController@index')->name('contact.index');
+Route::get('/dich-vu','ServiceController@index')->name('serveice.index');
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::group(['prefix' => 'manage'], function(){
+
+    Auth::routes(['register' => false]);
+
+    Route::group(['prefix'=> 'contact'], function(){
+        Route::get('/','Manage/ContactController@index')->name('manage.contact.index');
+        Route::delete('/{contact}','Manage/ContactController@destroy')->name('manage.contact.destroy');
+    });
+
+    Route::group(['prefix'=> 'profile'], function(){
+        Route::get('/','Manage/ProfileController@index')->name('manage.profile.index');
+        Route::put('/{profile}','Manage/ProfileController@update')->name('manage.profile.update');
+    });
+
+    Route::group(['prefix'=> 'blog'], function(){
+        Route::get('/','Manage/BlogController@index')->name('manage.blog.index');
+        Route::post('/','Manage/BlogController@store')->name('manage.blog.store');
+        Route::get('/create','Manage/BlogController@create')->name('manage.blog.create');
+        Route::get('/{blog}/edit','Manage/BlogController@edit')->name('manage.blog.edit');
+        Route::put('/{blog}','Manage/BlogController@update')->name('manage.blog.update');
+        Route::delete('/{blog}','Manage/BlogController@destroy')->name('manage.blog.destroy');
+    });
+   
 });
+
